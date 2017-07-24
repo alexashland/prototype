0ad4b1f0e9dcfea62d8d5795807bba1698b28ef8
alexashland <alexander-ashland@uiowa.edu>
2017-07-24 16:12:20 -0500

    added script for header

---
+++ assets/js/sample.js
@@ -0,0 +1,15 @@
+// var x = document.querySelectorAll("h3");
+// x[0].innerHTML = "work in progress";
+// x[1].innerHTML = "work in progress";
+// x[2].innerHTML = "work in progress";
+// x[3].innerHTML = "work in progress";
+// x[4].innerHTML = "work in progress";
+// x[5].innerHTML = "work in progress";
+// x[6].innerHTML = "work in progress";
+
+//
+// var x = document.querySelectorAll("h3");
+// var i;
+// for (i = 0; i < x.length; i++) {
+//   x[i].innerHTML = "hi";
+// }
