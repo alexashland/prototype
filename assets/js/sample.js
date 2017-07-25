00d82f8297f871753b397191e49cfc789fce1432
alexashland <alexander-ashland@uiowa.edu>
2017-07-25 11:27:27 -0500

    placed javascript script in header and added defer; added D3 to sample.js

--- assets/js/sample.js
+++ assets/js/sample.js
@@ -1,4 +1,8 @@
+<!-- Select a single header. -->
+
 // document.querySelector("h3").innerHTML = "work in progress";
+
+<!-- Identify select headers within a larger array. -->
 
 // var x = document.querySelectorAll("h3");
 // x[0].innerHTML = "work in progress";
@@ -9,9 +13,14 @@
 // x[5].innerHTML = "work in progress";
 // x[6].innerHTML = "work in progress";
 
-//
+<!-- Select all headers using a loop. -->
+
 // var changeHeader = document.querySelectorAll("h3");
 // var i;
 // for (i = 0; i < changeHeader.length; i++) {
 //   changeHeader[i].innerHTML = "work in progress";
 // }
+
+<!-- Use D3 to accomplish the same tasks. -->
+
+// d3.selectAll("p").style("background-color", "black");
