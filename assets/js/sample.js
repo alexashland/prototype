a2dfd3acbd722b7a839ea8a2839e615fa0a31890
alexashland <alexander-ashland@uiowa.edu>
2017-07-25 16:24:20 -0500

    saved

--- assets/js/sample.js
+++ assets/js/sample.js
@@ -4,9 +4,9 @@
 
 <!-- Identify select headers within a larger array. -->
 
-var changeHeader = document.querySelectorAll("h3");
+// var changeHeader = document.querySelectorAll("h3");
 // changeHeader[0].innerHTML = "work in progress";
-changeHeader[1].innerHTML = "work in progress";
+// changeHeader[1].innerHTML = "work in progress";
 // changeHeader[2].innerHTML = "work in progress";
 // changeHeader[3].innerHTML = "work in progress";
 // changeHeader[4].innerHTML = "work in progress";
@@ -24,12 +24,6 @@ changeHeader[1].innerHTML = "work in progress";
 <!-- Use D3 to accomplish the same tasks.  Select the first element you come across. -->
 
 // d3.select("h3").text("work in progress");
-
-
-<!-- Use D3 to accomplish the same tasks.  Select the first element you come across. -->
-
-// d3.selectAll("h3").text("work in progress");
-
 
 <!-- Use D3 to accomplish the same tasks.  Select all elements. -->
 
